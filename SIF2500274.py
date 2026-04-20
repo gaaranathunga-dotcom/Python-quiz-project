95122a377e6891a8ec906a6056a0d3ca928d4dd6
gaaranathunga-dotcom <gaaranathunga@gmail.com>
2026-04-20 11:31:29 +0530

    Add files via upload

---
+++ SIF2500274.py
@@ -0,0 +1,38 @@
+print('welcome to quiz game')
+name=int(input['enter your name'])
+print('Hello',name)
+print('lets start the quiz')
+
+score=0
+
+Q1(input['1.what is the longest river in Sri Lanka'])
+
+if Q1.lower('Mahawalli River')
+ print('correct')
+ score=score+1
+ else:
+  print('wrong')
+
+Q2(input['2.what is the tourisum side of Sri Lanka'])
+
+if Q2.lower('down south')
+  print('correct')
+  score=score+1
+  else:
+    print('wrong')
+
+Q3(input['3.what is the capital of Sri Lanka'])
+
+if Q3.lower('colombo')
+  print('correct')
+  score=score+1
+  else:
+    print('wrong')
+
+print('your score is:',score)
+if score ==3:
+  print('excellent')
+  elifscore ==2:
+    print('good job')
+    else:
+    print('Try egain')
